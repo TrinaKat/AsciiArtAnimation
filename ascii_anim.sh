c9ed25f86b74b128f78aeb413d85faad7496ff50
Katrina Wijaya <katrina@thewijayas.com>
2016-10-09 12:12:20 -0700

    Enabled repeating the animation (forever until quit)

--- ascii_anim.sh
+++ ascii_anim.sh
@@ -20,44 +20,54 @@ printf "\e[8;${screen_h};${screen_w}t"
 
 clear
 
-# Set variables for range of lines to print
-start_line=2  # Numbered starting from 1, Line 1 is page_lines #
-let end_line=page_lines+1
-buffer=0
-
 if [ -t 0 ]; then stty -echo -icanon -icrnl time 0 min 0; fi
 
-# While loop runs until the first line to print exceeds the number of file lines
-while [ $start_line -lt $line_num ]
+while (true)
 do
-  keypress="`cat -v`"
-  if [ "x$keypress" != "xq" ]
-  then
-    # If the last line to print exceeds the number of lines in the file, calculates the overflow and stores it in buffer var. Also sets end_line var to number of file lines
-    if [ $end_line -ge $line_num ]
+  # Set variables for range of lines to print
+  start_line=2  # Numbered starting from 1, Line 1 is page_lines #
+  let end_line=page_lines+1
+  buffer=0
+
+  # While loop runs until the first line to print exceeds the number of file lines
+  while [ $start_line -lt $line_num ]
+  do
+    keypress="`cat -v`"
+    if [ "x$keypress" != "xq" ]
     then
-      let buffer=end_line-line_num
-      end_line=line_num
+      # If the last line to print exceeds the number of lines in the file, calculates the overflow and stores it in buffer var. Also sets end_line var to number of file lines
+      if [ $end_line -ge $line_num ]
+      then
+        let buffer=end_line-line_num
+        end_line=line_num
+      fi
+
+      # Prints every 0.1 seconds
+      sleep 0.1
+      # Prints a range of lines. Lines are numbered from 1 onward, with sed printing [i, j)
+      sed -n "${start_line},${end_line}p" $file
+      # If buffer is not 0, then we reached end of file and there wasn't enough to fill the screen, print new lines
+      while [ $buffer -gt 0 ]
+      do
+        printf "\n"
+        let buffer=buffer-1
+      done
+
+      # Increment start and end line ranges
+      let start_line=start_line+page_lines
+      let end_line=end_line+page_lines
+    else
+      break
     fi
+  done
 
-    # Prints every 0.1 seconds
-    sleep 0.1
-    # Prints a range of lines. Lines are numbered from 1 onward, with sed printing [i, j)
-    sed -n "${start_line},${end_line}p" $file
-    # If buffer is not 0, then we reached end of file and there wasn't enough to fill the screen, print new lines
-    while [ $buffer -gt 0 ]
-    do
-      printf "\n"
-      let buffer=buffer-1
-    done
-
-    # Increment start and end line ranges
-    let start_line=start_line+page_lines
-    let end_line=end_line+page_lines
-  else
-    break
+  if [ $# -lt 2 ]; then break
+  elif [ $2 == "r" ]; then continue
+  else break
   fi
+
 done
+
 
 # The stty -echo option makes the shell stop displaying what you type with -echo
 stty echo
